81ba361002e58acd62026dd824fca3b1a4b1f3c2
J.A.Gentry <gentry.jon89@gmail.com>
2020-05-11 23:54:38 -0500

    Added TODOs and added taunt() to Computer

--- src/com/wargames/MainMenu.java
+++ src/com/wargames/MainMenu.java
@@ -5,6 +5,8 @@ import javafx.stage.Stage;
 
 public class MainMenu extends Application {
 
+    //TODO Need to make a main menu to select avatars, change backgrounds, and start game/ scene switch
+
     public static void main(String[] args) {
         launch(args);
     }
--- src/com/wargames/Player.java
+++ src/com/wargames/Player.java
@@ -4,5 +4,15 @@ import javafx.scene.image.ImageView;
 
 public class Player {
 
-    private ImageView player;
+    //TODO add cat image to the imageview
+    private static ImageView playerToken;
+
+    //TODO add 6 avatars to imageview
+    private static ImageView avatar1;
+    private static ImageView avatar2;
+    private static ImageView avatar3;
+    private static ImageView avatar4;
+    private static ImageView avatar5;
+    private static ImageView avatar6;
+
 }
