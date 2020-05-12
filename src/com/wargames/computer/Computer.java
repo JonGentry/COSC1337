791e8015a52374459964dd40948cd41b80ff161c
JAGentry <gentry.jon89@gmail.com>
2020-05-11 23:20:08 -0500

    Added packages and initial classes for OOP

---
+++ src/com/wargames/computer/Computer.java
@@ -0,0 +1,10 @@
+package com.wargames.computer;
+
+import javafx.scene.image.ImageView;
+
+public class Computer {
+
+    private ImageView computer;
+
+
+}
---
+++ src/com/wargames/player/Player.java
@@ -0,0 +1,8 @@
+package com.wargames.player;
+
+import javafx.scene.image.ImageView;
+
+public class Player {
+
+    private ImageView player;
+}
